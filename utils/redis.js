07cdca0b79de591d746847bddbc78ab03b960bae
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-28 05:49:07 +0300

    redisClient class

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import { createClient } from 'redis';
+
+
+class RedisClient {
+  constracture() {
+    this.client = createClient();
+    client.on('error', err => console.log('Redis Client Error', err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const data = promisify(this.client.get).bind(this.client)(key);
+    return data;
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.set)
+      .bind(this.client)(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
