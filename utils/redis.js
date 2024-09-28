118eec91cf800445fbe8016aa207442be437d6e0
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-28 06:52:07 +0300

    debug

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,12 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
+
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.isConnect = true;
-    this.getAsync = promisify(this.client.get).bind(this.client);
     this.client.on('error', (err) => {
-        console.error('Redis client failed to connect:', err.message || err.toString());
+        console.error('Redis client failed to connect:', err.message);
         this.isConnect = false;
     });
     this.client.on('connect', () => {this.isConnect = true;});
@@ -17,18 +17,19 @@ class RedisClient {
   }
 
   async get(key) {
-    const value = await this.getAsync(key);
-    return value;
+    const data = promisify(this.client.get).bind(this.client)(key);
+    return data;
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, 'EX', duration);
+    await promisify(this.client.set)
+      .bind(this.client)(key, value, 'EX', duration);
   }
 
   async del(key) {
-    await this.client.del(key);
+    await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
 
-const redisClient = new RedisClient();
+export const redisClient = new RedisClient();
 export default redisClient;
