f8c16228749ea05070cf5eacd5b92b9903a72e1e
sabah-hue <sabah.abdelbaset@gmail.com>
2024-10-01 04:48:13 +0300

    output issue

--- utils/redis.js
+++ utils/redis.js
@@ -1,26 +1,31 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
+// Redis utils
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => {
-      console.log(err);
+    this.client.on('error', (e) => {
+      console.log(`Redis client not connected to server: ${e}`);
     });
   }
 
+  // returns true when connection success otherwise, false
   isAlive() {
-    if(this.client.connected)
-      return false;
-    return true;
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
   }
 
+  // returns the Redis value stored for this key
   async get(key) {
-    const data = await promisify(this.client.get).bind(this.client);
+    const data = promisify(this.client.get).bind(this.client);
     const value = await data(key);
     return value;
   }
 
+  // store in redis
   async set(key, value, duration) {
     const data = promisify(this.client.set).bind(this.client);
     // await data(key, value, 'EX', duration);
@@ -28,11 +33,12 @@ class RedisClient {
     await this.client.expire(key, duration);
   }
 
+  // remove the value in Redis for given key
   async del(key) {
-    const data = promisify(this.client.DEL).bind(this.client);
+    const data = promisify(this.client.del).bind(this.client);
     await data(key);
   }
 }
 
 const redisClient = new RedisClient();
-export default redisClient;
+module.exports = redisClient;
