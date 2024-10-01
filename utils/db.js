f1d4f38bcb8e253891d5f5314070c94171d029b9
sabah-hue <sabah.abdelbaset@gmail.com>
2024-10-01 05:35:08 +0300

    eslint fix

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
@@ -9,8 +9,8 @@ class DBClient {
   constructor() {
     this.client = new MongoClient(`mongodb://${HOST}:${PORT}`);
     this.client.connect()
-               .then(() => this.db = this.client.db(`${DATABASE}`))
-               .catch(e => console.log(e));
+      .then(() => this.db = this.client.db(`${DATABASE}`))
+      .catch((e) => console.log(e));
   }
 
   // true when connection to MongoDB is a success otherwise, false
